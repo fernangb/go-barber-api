062a899fb3ca279b5a4a39a265c0260c9b5a83a0
fernangb <gfsouza@poli.ufrj.br>
2020-08-07 18:11:31 -0300

    Criando controllers nas rotas

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,29 +1,10 @@
 import { Router } from 'express';
-import {container} from 'tsyringe';
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import SessionsController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
+const sessionsController = new SessionsController();
 
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
 
-  const authenticateUser = container.resolve(AuthenticateUserService);
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  });
-
-  const responseUser = {
-    id: user.id,
-    name: user.name,
-    avatar: user.avatar,
-    email,
-    created_at: user.created_at,
-    updated_at: user.updated_at,
-  };
-
-  return response.json({ user: responseUser, token });
-});
+sessionsRouter.post('/', sessionsController.create);
 
 export default sessionsRouter;
