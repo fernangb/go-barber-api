8a66a389715e7a606e9dfa78550e357364a2380e
fernangb <gfsouza@poli.ufrj.br>
2020-08-08 15:55:15 -0300

    Atualização de Avatar

---
+++ src/modules/users/services/UpdateUserAvatar.spec.ts
@@ -0,0 +1,66 @@
+import "reflect-metadata"
+import AppError from '@shared/errors/AppError';
+import UpdateUserAvatarService from './UpdateUserAvatarService';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+
+describe('UpdateUserAvatar', () => {
+    it('should be able to create a new user',async() => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeStorageProvider = new FakeStorageProvider();
+        const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
+
+        const user = await fakeUsersRepository.create({
+            name: 'Fulano',
+            email: 'fulano@detal.com',
+            password: '123456'
+        });
+
+        await updateUserAvatar.execute({
+            user_id: user.id,
+            avatarFileName: 'avatar.jpg',
+        }); 
+
+        expect(user.avatar).toBe('avatar.jpg');
+    });
+
+    it('should not be able to update avatar from non exixting user',async() => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeStorageProvider = new FakeStorageProvider();
+        const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
+
+        expect(updateUserAvatar.execute({
+            user_id: 'non-existing-user',
+            avatarFileName: 'avatar.jpg',
+        })).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should delete old avatar when updating a new one' ,async() => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeStorageProvider = new FakeStorageProvider();
+
+        const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+        const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
+
+        const user = await fakeUsersRepository.create({
+            name: 'Fulano',
+            email: 'fulano@detal.com',
+            password: '123456'
+        });
+
+        await updateUserAvatar.execute({
+            user_id: user.id,
+            avatarFileName: 'avatar.jpg',
+        }); 
+
+        await updateUserAvatar.execute({
+            user_id: user.id,
+            avatarFileName: 'avatar2.jpg',
+        }); 
+
+        expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
+        expect(user.avatar).toBe('avatar2.jpg');
+    });
+
+});
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -5,6 +5,7 @@ import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 interface IRequest {
   user_id: string;
@@ -15,7 +16,10 @@ interface IRequest {
 class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')  
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')  
+    private storageProvider: IStorageProvider
   ){
   }
 
@@ -27,15 +31,12 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFileName;
+    const filename = await this.storageProvider.saveFile(avatarFileName);
+
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
     return user;
