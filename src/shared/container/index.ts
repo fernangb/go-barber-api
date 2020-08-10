f350215243bcd2f27648dfd6938f9a683840ef2e
fernangb <gfsouza@poli.ufrj.br>
2020-08-10 12:27:50 -0300

    ajuestes de erros no server

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,5 @@
 import {container} from 'tsyringe';
-import '@models/users/providers';
+import '@modules/users/providers';
 import './providers';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
