998d6b7947d37ea6447623b9f07524cb4d6c624f
fernangb <gfsouza@poli.ufrj.br>
2020-08-11 21:16:17 -0300

    Iniciando recuperação de senha

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,28 @@
+import "reflect-metadata"
+import AppError from '@shared/errors/AppError';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover the password using the email',async() => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(fakeUsersRepository, fakeMailProvider);
+
+    await fakeUsersRepository.create({
+      name: 'Fulano',
+      email: 'fulano@detal.com',
+      password: '123456',
+    })
+
+    await sendForgotPasswordEmail.execute({
+      email: 'fulano@detal.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+});
